cf9b0ba136a60ccc2b8f2a60a3dde244f1cfd54b
Khoa <cuonglieu3256@gmail.com>
2020-12-15 23:35:13 +0700

    Update IOT Dash Board

--- IOT_dashboard/settings.py
+++ IOT_dashboard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = BASE_DIR, 'static'
 LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/iot/dashboard/'
