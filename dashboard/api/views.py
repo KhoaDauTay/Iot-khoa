f55705400c4825f47b5273f97cb662fe17cd82a6
Khoa <cuonglieu3256@gmail.com>
2020-12-16 08:39:28 +0700

    Update project

--- dashboard/api/views.py
+++ dashboard/api/views.py
@@ -12,16 +12,6 @@ class NodeViewSet(viewsets.ModelViewSet):
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
 
-    def retrieve(self, request, pk=id):
-        node = get_object_or_404(self.queryset, pk=pk)
-        node.temperature = randint(80, 100)
-        node.humidity = randint(20, 30)
-        node.moisture = randint(60, 80)
-        node.light = randint(80, 100)
-        node.save()
-        serializer = self.serializer_class(node)
-        return Response(serializer.data)
-
 
 class GardenViewSet(viewsets.ModelViewSet):
     queryset = Garden.objects.all()
